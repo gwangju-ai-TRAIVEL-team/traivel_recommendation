034ccba5b673afd1a45c1b7e549fe283d0cb9dd6
SeongMin Kim <ksmksj5265@gmail.com>
2020-09-13 02:11:08 +0900

    remove

--- traivel_web/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Input(models.Model) :
-    region = models.CharField(max_length=18)
-    keyword = models.CharField(max_length=20)
-    Type = models.CharField(max_length=20)
