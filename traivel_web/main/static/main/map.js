c59e591c1abe4838b79175bba34efe1c14a6548d
yunminhui <pompo8918@gmail.com>
2020-09-15 22:48:07 +0900

    marker create

--- traivel_web/main/static/main/map.js
+++ traivel_web/main/static/main/map.js
@@ -39,13 +39,14 @@ function setCenter(region){
 }
 
 
-// for (i =0; i<데이터.length; i++ ) {
-//     //지도에 마커를 생성하고 표시
-//     var marker = new kakao.maps.Marker({
-//         position: new kakao.maps.LatLng(데이터[i][3], 데이터[i][4]),
-//         map: map //마커를 표시할 지도 객체
-//         });
-//     marker.setMap(map);
+function create_marker(lat,lon) {
+    
+    //지도에 마커를 생성하고 표시
+    var marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(lat, lon),
+        map: map //마커를 표시할 지도 객체
+        });
+    marker.setMap(map);
 
 //     var iwContent = '<div style="padding:5px;">출발! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 //     //인포윈도우 표시 위치입니다
@@ -57,7 +58,7 @@ function setCenter(region){
                         
 //     // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
 //     infowindow.open(map, marker); 
+}
 
-// }
 
 
